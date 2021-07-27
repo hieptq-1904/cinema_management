cddc74069789a50c34e9c7710c9c7a5974fe5136
Vominhchau2011 <vmchauptit@gmail.com>
2021-07-27 18:46:33 +0700

    Build template for project

---
+++ mvc/Views/Homepage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+    include_once "layout/head.php";
+?>
+    <title>Home Page</title>
+</head>
+<body id="page-top">
+
+        <?php
+            include_once "layout/Sidebar.php";
+        ?>
+</body>
